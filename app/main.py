df9f1ce635e70e4d251c265fd99eb3e2b5dd68e9
Flux <aayyuusshh.45@gmail.com>
2024-10-03 14:23:01 +0530

    second stage

--- app/main.py
+++ app/main.py
@@ -8,7 +8,14 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    print("Server is listening to port 4221...")
+    # server_socket.accept() # wait for client
+
+    while True:
+        client_socket, address = server_socket.accept()
+        print(f"Accepted connection from {address}")
+        client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        client_socket.close()
 
 
 if __name__ == "__main__":
