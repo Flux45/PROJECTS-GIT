6ea19c015df73bae65d8701d3f0ed913c2d9ea2c
Flux <aayyuusshh.45@gmail.com>
2024-10-03 18:57:58 +0530

    404 added

--- app/main.py
+++ app/main.py
@@ -13,29 +13,16 @@ def main():
     server_socket = socket.create_server(("localhost", 4221))
     client, addr = server_socket.accept()
     print("Server is listening to port 4221...")
+    while True:
+        data: str = client.recv(1024).decode()
+        request_data: list[str] = data.split("\r\n")
+        response: bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
+        if request_data[0].split(" ")[1] != '/':
+            response: bytes = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        client.send(response)
 
-    with client:
-        while True:
-            data = client.recv(1024)
-            request_data = data.decode().split("\r\n")
-            response = b"HTTP/1.1 200 OK\r\n\r\n"
-            #  ['GET / HTTP/1.1', 'Host: localhost:4221', '', '']
-            if not data:
-                break
-            client.send(b"HTTP/1.1 200 OK\r\n\r\n")
-            if request_data[0].split(" ")[1] != "/":
-                response = b"HTTP/1.1 404 Not Found\r\n\r\n"
-            client.send(response)
-
-    # data: str = client.recv(1024).decode()
-    # request_data: list[str] = data.split("\r\n")
-    # response: bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
-    # if request_data[0].split(" ")[1] != '/':
-    #     response: bytes = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
-    # client.send(response)
-    #
-    # client.close()
-    # server_socket.close()
+        client.close()
+        server_socket.close()
 
 
     # server_socket.accept() # wait for client
@@ -46,8 +33,6 @@ def main():
     #
     #     client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     #     client_socket.close()
-
-
 
 
 
