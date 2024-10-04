7c47a7ec86ddfd05016b97a562dbd35710caec84
Flux <aayyuusshh.45@gmail.com>
2024-10-04 14:38:23 +0530

    Response With Body Added

--- app/main.py
+++ app/main.py
@@ -1,39 +1,54 @@
-import socket  # noqa: F401
+import socket
 
-def parse_request(request_data):
-    lines = request_data.split("\r\n")
+def handles_request(client):
+    print("inside handles request")
+    try:
+        data = client.recv(1024)
+        if not data:
+            return
+        response = parse_request(data)
+        client.sendall(response)
+    except Exception as e:
+        print(f"Error handling request: {e}")
+    finally:
+        client.close()
 
+
+def parse_request(data):
+    print("inside parse request")
+    try:
+        data: list[str] = data.decode().split("\r\n")
+        request_data = data[0]
+        method, target = request_data.split(" ")[:2]
+        if method == "GET" and target == "/":
+            response: bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
+
+        elif target.startswith("/echo"):
+            content = target.split("/")[1]
+            response: bytes = \
+                f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(content)}\r\n\r\n{content}".encode()
+        else:
+            response: bytes = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+
+        return response
+    except Exception as e:
+        print(f"Error parsing request: {e}")
+        return "HTTP/1.1 400 Bad Request\r\n\r\n"
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221))
-    client, addr = server_socket.accept()
-    print("Server is listening to port 4221...")
-    while True:
-        data: str = client.recv(1024).decode()
-        request_data: list[str] = data.split("\r\n")
-        response: bytes = "HTTP/1.1 200 OK\r\n\r\n".encode()
-        if request_data[0].split(" ")[1] != '/':
-            response: bytes = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
-        client.send(response)
-
-        client.close()
+    server_socket.listen()
+    try:
+        while True:
+            client, addr = server_socket.accept()
+            print("Server is listening to port 4221...")
+            handles_request(client)
+    except Exception as e:
+        print(f"Server Error: {e}")
+    finally:
         server_socket.close()
-
-
-    # server_socket.accept() # wait for client
-
-    # while True:
-    #     client_socket, address = server_socket.accept()
-    #     print(f"Accepted connection from {address}")
-    #
-    #     client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    #     client_socket.close()
-
 
 
 if __name__ == "__main__":
