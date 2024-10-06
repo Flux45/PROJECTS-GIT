5234a00a8f255fa6f3830eaba5eb5fd46c51e450
Flux <aayyuusshh.45@gmail.com>
2024-10-06 12:58:19 +0530

    Read request body

--- app/main.py
+++ app/main.py
@@ -104,7 +104,7 @@ def parse_request(data, client):
                 print("going in")
                 body = bodyyy
                 print("BODY   : " + body)
-                with open(path[1:], "wb") as f:
+                with open(file_location, "wb") as f:
                     print("opened")
                     f.write(name.encode())
                     print("wrote")
